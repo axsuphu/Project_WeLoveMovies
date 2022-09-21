a57e98843b3c0d6e8e6f01da360adc4a9652c86a
Alyssa Suphunthuchat <axsuphu@gmail.com>
2022-09-21 15:32:55 -0700

    completed reviews router, controller, and service

--- src/reviews/reviews.controller.js
+++ src/reviews/reviews.controller.js
@@ -11,7 +11,33 @@ async function read(req, res, next) {
   res.json({ data });
 }
 
+async function reviewExists(req, res, next) {
+  const review = await service.read(req.params.reviewId);
+  if (review) {
+    res.locals.review = review;
+    return next();
+  }
+  next({ status: 404, message: `Review cannot be found.` });
+}
+
+async function update(req, res, next) {
+  const updatedReview = {
+    ...req.body.data,
+    review_id: res.locals.review.review_id,
+  };
+  await service.update(updatedReview);
+  const data = await service.addCritics(updatedReview.review_id);
+  res.json({ data });
+}
+
+async function destroy(req, res) {
+  await service.destroy(res.locals.review.review_id);
+  res.sendStatus(204);
+}
+
 module.exports = {
   list: asyncErrorBoundary(list),
   read: asyncErrorBoundary(read),
+  update: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(update)],
+  delete: [asyncErrorBoundary(reviewExists), asyncErrorBoundary(destroy)],
 };
--- src/reviews/reviews.router.js
+++ src/reviews/reviews.router.js
@@ -2,8 +2,13 @@ const router = require("express").Router();
 const controller = require("./reviews.controller");
 const methodNotAllowed = require("../errors/methodNotAllowed");
 
-router.route("/").get(controller.list).all(methodNotAllowed);
+router
+  .route("/:reviewId")
+  .get(controller.read)
+  .put(controller.update)
+  .delete(controller.delete)
+  .all(methodNotAllowed);
 
-router.route("/:reviewId").get(controller.read).all(methodNotAllowed);
+router.route("/").get(controller.list).all(methodNotAllowed);
 
 module.exports = router;
