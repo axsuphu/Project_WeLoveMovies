7a9af49428d06009464a53fad93d1215986bde79
Alyssa Suphunthuchat <axsuphu@gmail.com>
2022-09-23 13:17:40 -0700

    reconfigure migrations

--- src/db/migrations/20220202104739_createCriticsTable.js
+++ src/db/migrations/20220202104739_createCriticsTable.js
@@ -1,5 +1,5 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("critics", (table) => {
+  return knex.schema.createTable("critics", table => {
     table.increments("critic_id").primary();
     table.string("preferred_name");
     table.string("surname");
